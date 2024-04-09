d8d246a5c76be077ffae43669ca0b1715155b9d6
Dipokal HHJ <hhj@devent.kr>
2024-04-09 18:40:53 +0900

    feat: move mouse

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -1,41 +1,142 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import logo from "./logo.svg";
 import { css } from "@emotion/react";
 
 function Canvas() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [mousePosition, setMousePosition] = useState({
+    down: {
+      x: 0,
+      y: 0,
+    },
+    current: {
+      x: 0,
+      y: 0,
+    },
+  });
+  const [canvasCenter, setCanvasCenter] = useState({
+    down: {
+      x: window.innerWidth / 2,
+      y: window.innerHeight / 2,
+    },
+    current: {
+      x: window.innerWidth / 2,
+      y: window.innerHeight / 2,
+    },
+  });
+  const [isMouseMove, setIsMouseMove] = useState(false);
+
   const resizeCanvas = () => {
     const canvas = canvasRef.current as HTMLCanvasElement;
 
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+
+    setCanvasCenter({
+      down: {
+        x: window.innerWidth,
+        y: window.innerHeight,
+      },
+      current: {
+        x: window.innerWidth,
+        y: window.innerHeight,
+      },
+    });
   };
 
-  const drawCoordinate = () => {
+  const clearCanvas = () => {
+    const canvas = canvasRef.current as HTMLCanvasElement;
+    const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  };
+
+  const drawCoordinate = ({ x, y }: { x: number; y: number }) => {
+    clearCanvas();
     const canvas = canvasRef.current as HTMLCanvasElement;
     const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 
     ctx.lineWidth = 0.5;
 
     ctx.beginPath();
-    ctx.moveTo(0, canvas.height / 2);
-    ctx.lineTo(canvas.width, canvas.height / 2);
+    ctx.moveTo(0, y + canvas.height / 2);
+    ctx.lineTo(canvas.width, y + canvas.height / 2);
     ctx.stroke();
 
     ctx.beginPath();
-    ctx.moveTo(canvas.width / 2, 0);
-    ctx.lineTo(canvas.width / 2, canvas.height);
+    ctx.moveTo(x + canvas.width / 2, 0);
+    ctx.lineTo(x + canvas.width / 2, canvas.height);
     ctx.stroke();
   };
 
   const initCanvas = () => {
     resizeCanvas();
-    drawCoordinate();
+    drawCoordinate({ x: 0, y: 0 });
+  };
+
+  const handleMouseDown = (e: any) => {
+    canvasCenter.down.x = canvasCenter.current.x;
+    canvasCenter.down.y = canvasCenter.current.y;
+    mousePosition.down.x = e.clientX;
+    mousePosition.down.y = e.clientY;
+
+    setCanvasCenter({
+      ...canvasCenter,
+    });
+
+    setMousePosition({
+      ...mousePosition,
+    });
+
+    setIsMouseMove(true);
+  };
+
+  const handleMouseUp = () => {
+    setIsMouseMove(false);
+  };
+
+  const handleMouseMove = (e: any) => {
+    setMousePosition({
+      ...mousePosition,
+      ["current"]: {
+        x: e.clientX,
+        y: e.clientY,
+      },
+    });
+  };
+
+  const initEvents = () => {
+    const canvas = canvasRef.current as HTMLCanvasElement;
+    canvas.addEventListener("mousedown", handleMouseDown);
+    canvas.addEventListener("mouseup", handleMouseUp);
+    canvas.addEventListener("mousemove", handleMouseMove);
   };
 
   useEffect(() => {
     initCanvas();
+    initEvents();
   }, []);
+
+  useEffect(() => {
+    if (isMouseMove) {
+      canvasCenter.current.x =
+        canvasCenter.down.x + mousePosition.current.x - mousePosition.down.x;
+      canvasCenter.current.y =
+        canvasCenter.down.y + mousePosition.current.y - mousePosition.down.y;
+
+      setCanvasCenter({
+        ...canvasCenter,
+      });
+    }
+  }, [mousePosition]);
+
+  useEffect(() => {
+    if (isMouseMove) {
+      drawCoordinate({
+        x: canvasCenter.current.x - window.innerWidth / 2,
+        y: canvasCenter.current.y - window.innerHeight / 2,
+      });
+    }
+  }, [canvasCenter]);
 
   return <canvas ref={canvasRef}></canvas>;
 }
